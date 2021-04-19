d83fcdfd92076a879e507c56fd199e44ea181104
Raymond Hunce <hunce18c@gmail.com>
2021-04-19 12:02:10 -0400

    Complete 2-globals

--- 1-node-tutorial/2-globals.js
+++ 1-node-tutorial/2-globals.js
@@ -1,12 +1,16 @@
 // GLOBALS  - NO WINDOW !!!!
 
+// These are some of Node's global variables....not all of them.
+
 // __dirname  - path to current directory
 // __filename - file name
 // require    - function to use modules (CommonJS)
 // module     - info about current module (file)
 // process    - info about env where the program is being executed
 
-console.log(__dirname)
+console.log('dirname: ', __dirname);
+console.log('filename: ', __filename);
 setInterval(() => {
-  console.log('hello world')
-}, 1000)
+  console.log('hello world');
+}, 1000);
+// console.log('process: ', process);
