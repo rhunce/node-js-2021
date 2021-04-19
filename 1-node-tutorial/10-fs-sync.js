b51db2035ed7966a5451251a1254f5242a0ace34
Raymond Hunce <hunce18c@gmail.com>
2021-04-19 15:18:08 -0400

    Complete fs modules 10 and 11

--- 1-node-tutorial/10-fs-sync.js
+++ 1-node-tutorial/10-fs-sync.js
@@ -1,12 +1,16 @@
-const { readFileSync, writeFileSync } = require('fs')
-console.log('start')
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
+const { readFileSync, writeFileSync } = require('fs');
+
+console.log('start');
+
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
 
 writeFileSync(
   './content/result-sync.txt',
-  `Here is the result : ${first}, ${second}`,
-  { flag: 'a' }
-)
-console.log('done with this task')
-console.log('starting the next one')
+  `Here is the result : ${first}, ${second} \n`,
+  { flag: 'a' } // This flag has node append the data in the second argument to the file in the first argument, as opposed to overwriting the file contents.
+);
+
+console.log('done with this task');
+
+console.log('starting the next one');
--- 1-node-tutorial/11-fs-async.js
+++ 1-node-tutorial/11-fs-async.js
@@ -1,29 +1,32 @@
-const { readFile, writeFile } = require('fs')
+const { readFile, writeFile } = require('fs');
 
-console.log('start')
+console.log('start');
+
 readFile('./content/first.txt', 'utf8', (err, result) => {
   if (err) {
-    console.log(err)
-    return
+    console.log(err);
+    return;
   }
-  const first = result
+  const first = result;
   readFile('./content/second.txt', 'utf8', (err, result) => {
     if (err) {
-      console.log(err)
-      return
+      console.log(err);
+      return;
     }
-    const second = result
+    const second = result;
     writeFile(
       './content/result-async.txt',
-      `Here is the result : ${first}, ${second}`,
+      `Here is the result : ${first}, ${second}\n`,
+      { flag: 'a' },
       (err, result) => {
         if (err) {
-          console.log(err)
-          return
+          console.log(err);
+          return;
         }
-        console.log('done with this task')
+        console.log('done with this task');
       }
-    )
-  })
-})
-console.log('starting next task')
+    );
+  });
+});
+
+console.log('starting next task');
