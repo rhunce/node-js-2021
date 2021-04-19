3aea57bbde109273bbc22f6cf195dbcadae1d1ec
Raymond Hunce <hunce18c@gmail.com>
2021-04-19 12:51:52 -0400

    Complete 3-modules to 7-mind-grenades

--- 1-node-tutorial/3-modules.js
+++ 1-node-tutorial/3-modules.js
@@ -1,9 +1,17 @@
 // CommonJS, every file is module (by default)
 // Modules - Encapsulated Code (only share minimum)
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-const data = require('./6-alternative-flavor')
-require('./7-mind-grenade')
-sayHi('susan')
-sayHi(names.john)
-sayHi(names.peter)
+
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+
+// console.log(names);
+// console.log(sayHi);
+// console.log(data);
+// console.log(require('./7-mind-grenade'));
+
+require('./7-mind-grenade');
+
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);
