ca6c0be004138659e0247e2922d5e85dcd9eda85
Raymond Hunce <hunce18c@gmail.com>
2021-04-19 20:30:11 -0400

    Complete 16-streams

--- 1-node-tutorial/16-streams.js
+++ 1-node-tutorial/16-streams.js
@@ -1,13 +1,17 @@
-const { createReadStream } = require('fs')
+const { createReadStream } = require('fs');
 
-// default 64kb
+// default 64kb buffer size
 // last buffer - remainder
 // highWaterMark - control size
-// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
-// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
-const stream = createReadStream('./content/big.txt')
+// const stream = createReadStream('./content/big.txt');
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 });
+// const stream = createReadStream('./content/big.txt', { encoding: 'utf8' });
+const stream = createReadStream('./content/big.txt', {
+  highWaterMark: 90000,
+  encoding: 'utf8',
+});
 
-stream.on('data', (result) => {
-  console.log(result)
-})
-stream.on('error', (err) => console.log(err))
+stream.on('data', (chunk) => {
+  console.log(chunk);
+});
+stream.on('error', (err) => console.log(err));
