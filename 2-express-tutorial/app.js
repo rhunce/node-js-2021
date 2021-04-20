b6b3311e6c803992618028f194385cab806be68b
Raymond Hunce <hunce18c@gmail.com>
2021-04-20 11:59:46 -0400

    Complete 2-express-tutorial 6-basic-json

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const path = require('path');
+const { products } = require('./data');
 const app = express();
 
-// set up static and middleware
-app.use(express.static('./public'));
-
-// app.get('/', (req, res) => {
-//   res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-// });
-
-app.all('*', (req, res) => {
-  res.status(404).send('Page Not Found');
+app.get('/', (req, res) => {
+  res.json(products);
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}....`);
+  console.log(`Server listening on port ${PORT}...`);
 });
