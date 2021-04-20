2704ae489df8882193e12fad294015e6298420dc
Raymond Hunce <hunce18c@gmail.com>
2021-04-19 20:07:37 -0400

    Complete 14-request-event

--- 1-node-tutorial/app.js
+++ 1-node-tutorial/app.js
@@ -1,1 +1,18 @@
 console.log('Node Tutorial');
+
+const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
+
+// Using Event Emitter API
+const server = http.createServer();
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on('request', (req, res) => {
+  console.log('Request made');
+  res.end('Welcome');
+});
+
+server.listen(5000);
